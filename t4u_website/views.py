1489cdbb8be7b0ea9f1546adbabe5ce8d81e6aba
Patrick2428 <ptietz17@gmail.com>
2021-09-05 18:25:23 +0200

    templates and routes

---
+++ t4u_website/views.py
@@ -0,0 +1,13 @@
+from os import RTLD_NODELETE
+from django import template
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
